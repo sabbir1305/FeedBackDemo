09651807c8da2d61a6b1498aefcab0799719d641
sabbir1305 <sabbir.ove@gmail.com>
2020-11-27 14:50:36 +0600

    Migration Added

---
+++ FeedBackDemo/Models/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace FeedBackDemo.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public string CreatedBy { get; set; }
+        [ForeignKey("CreatedBy")]
+        public ApplicationUser User { get; set; }
+
+    }
+}
