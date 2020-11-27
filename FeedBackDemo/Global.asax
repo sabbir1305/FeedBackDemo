999e64a9a6412ba729d501f149a5f6fbb3dd42e4
sabbir1305 <sabbir.ove@gmail.com>
2020-11-27 14:10:11 +0600

    Add project files.

---
+++ FeedBackDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FeedBackDemo.MvcApplication" Language="C#" %>
---
+++ FeedBackDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FeedBackDemo.Startup))]
+namespace FeedBackDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
